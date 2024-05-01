bf36aa2aee75b60f477bb673b7347f66a2eb515e
Igor Blinnikov <igor.blinnikov@gmail.com>
2024-05-01 21:10:13 +0300

    feat(pg): make pg connection

--- postgres-connection/src/main.rs
+++ postgres-connection/src/main.rs
@@ -3,5 +3,19 @@ use postgres::{Client, NoTls, Error};
 fn main() -> Result<(), Error> {
     println!("Hello, world!");
 
+    let host = "postgres://postgres:postgres@localhost:5432";
+    let db = "travel";
+
+    let connection_str = format!("{host}/{db}");
+
+    let mut client = Client::connect(connection_str.as_str(), NoTls)?;
+
+    client.batch_execute("
+        CREATE TABLE IF NOT EXISTS planes (
+            id SERIAL PRIMARY KEY,
+            model TEXT
+        )
+    ")?;
+
     Ok(())
 }
