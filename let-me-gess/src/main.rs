cc8cd5c7b2d5152148e4efd26f2d2ec9b4498f88
Igor Blinnikov <igor.blinnikov@gmail.com>
2024-05-06 00:19:05 +0300

    feat(guess): game complete

--- let-me-gess/src/main.rs
+++ let-me-gess/src/main.rs
@@ -1,15 +1,42 @@
+use std::cmp::Ordering;
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Try to guess the number!");
 
-    println!("Please enter your guess:");
+    let secret = rand::thread_rng().gen_range(1..=100);
+    let mut attempts = 1;
 
-    let mut guess = String::new();
+    loop {
+        println!("Please enter your guess:");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the line!");
+        let mut guess = String::new();
 
-    println!("Your guess is: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read the line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(number) => number,
+            Err(_) => continue,
+        };
+
+        println!("Your guess is: {guess}");
+
+        match guess.cmp(&secret) {
+            Ordering::Less => {
+                println!("Too small!");
+                attempts += 1;
+            },
+            Ordering::Greater => {
+                println!("Too big!");
+                attempts += 1;
+            },
+            Ordering::Equal => {
+                println!("You win!\nYou've spent {attempts} attempts\nBye!");
+                break
+            }
+        }
+    }
 }
